747a98eab4c71f4071e0221fe91252b92e10f46a
Pabitra Adhikari <pabitraadhikari@gmail.com>
2014-04-18 23:50:28 -0700

    wait sytem call

--- src/wait/wait.c
+++ src/wait/wait.c
@@ -1,7 +1,54 @@
+#include<sys/types.h>
+#include<sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char * argv)
 {
-	return 0;
+
+	pid_t pid;
+	char * message;
+	int n;
+	int exit_code;
+	
+	printf("Fork programming starting\n");
+	pid = fork();
+	switch(pid){
+		
+		case -1:
+			perror("fork failed");
+			exit(1);
+		case 0:
+			message = "This is the child process";
+			n = 5;
+			exit_code = 37;
+			break;
+		default:
+			message = "This is the parent";
+			n = 3;
+			exit_code = 0;
+			break;		
+	}
+	
+	for(; n>0; n--){
+		puts(message);
+		sleep(1);
+	}	
+	
+	//the program waits for the child process to finish
+	if(pid!=0){
+	
+		int stat_val;
+		pid_t child_pid;
+		
+		child_pid = wait(&stat_val);
+		
+		printf("child has finished: PID = %d\n", child_pid);
+		if(WIFEXITED(stat_val))
+			printf("Child exited with code %d\n", WEXITSTATUS(stat_val));
+		else
+			printf("Child terminated abnormally\n");	
+	}
+	
+	exit(exit_code);
 }
